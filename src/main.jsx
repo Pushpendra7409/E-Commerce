9e71955358773addd07885ed6fbb462ccaa69a47
Pushpendra7409 <pushpendra7409@gmail.com>
2024-09-21 16:31:35 +0530

    Adding axios and context

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import Context from './utils/Context.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Context>
+    <BrowserRouter>
+       <App />
+    </BrowserRouter>,
+  </Context>
 )
