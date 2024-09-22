b114c89c9a7f80241d643c2ccfc7ba75bf196cf0
Pushpendra7409 <pushpendra7409@gmail.com>
2024-09-22 09:17:26 +0530

    Single product details

--- src/Components/Details.jsx
+++ src/Components/Details.jsx
@@ -1,22 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import axios from '../utils/axios';
+import React, { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom'
+import Loading from './Loading';
 
 const Details = () => {
-  return (
+  const [products, setproduct] = useState(null);
+  const {id} = useParams();
+  console.log(id);
+  const getsingleproduct = async () => {
+    try{
+         const {data} = await axios.get(`/products/${id}`)
+         setproduct(data);
+    }catch{
+      console.log(error);
+    }
+  } 
+
+  useEffect(() => {getsingleproduct()}, [])
+
+  return products ?(
     <div className='w-[70%] flex h-full justify-between items-center m-auto p-[10%]'>
 
-     <img className='object-contain w-[40%] h-[80%]' src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg" alt="Product Image" />
+     <img className='object-contain w-[40%] h-[80%]' src={`${products.image}`} alt="Product Image" />
      <div className='content w-[50%]' >
-        <h1 className='text-4xl'>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</h1>
-        <h3 className='text-zinc-400 my-5'>Men's clothing</h3>
-        <p className='mb-3'>Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday</p>
-        <h2 className='text-red-300 mb-[5%]'>Pricing: $100.00</h2>
+        <h1 className='text-4xl'>{products.title}</h1>
+        <h3 className='text-zinc-400 my-5'>{products.category}</h3>
+        <p className='mb-3'>{products.description}</p>
+        <h2 className='text-red-300 mb-[5%]'>{products.price}</h2>
         <Link className='py-2 px-5 border rounded border-blue-200 text-blue-300 mr-5'>Edit</Link>
         <Link className='py-2 px-5 border rounded border-red-200 text-red-300'>Delete</Link>
      </div>
 
     </div>
-  )
+  ): (<Loading/>
+  );
 }
 
 export default Details
