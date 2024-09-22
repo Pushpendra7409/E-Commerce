389102dc1962869fd15a36f892ea7f419f9aa13f
Pushpendra7409 <pushpendra7409@gmail.com>
2024-09-22 10:09:03 +0530

    Applying Filter path not actual Filter products

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ProductContext } from '../utils/Context';
+import { Link } from 'react-router-dom';
 
 const Nav = () => {
+    const [products] = useContext(ProductContext, []);
+
+    let distinct_category = 
+         products && products.reduce((acc, cv) => [...acc, cv.category], []);
+          distinct_category = [...new Set(distinct_category)];
+        
+          const color = () => {
+            return `rgba(${(Math.random() * 255) .toFixed()},${(Math.random() * 255) .toFixed()},${(Math.random() * 255) .toFixed()},0.4)`;
+          };
+          console.log(color());
+
   return (
     
     <nav className='w-[15%] h-full bg-zinc-50 flex flex-col items-center pt-5'>
@@ -14,21 +27,16 @@ const Nav = () => {
       <h1 className='text-2xl mb-3 w-[80%]'>Category Filter
       </h1>
 
-      <ul className='w-[80%]'>
+      <div className='w-[80%]'>
 
-        <li className='flex items-center mb-3'>
-        <span className='rounded-full mr-2 w-[15px] h-[15px] bg-blue-100 '></span>
-        Cat 1</li>
+      {distinct_category.map((c, i) =>  (
+        <Link key={i} to= {`/?category=${c}`}className='flex items-center mb-3'>
+        <span style = {{backgroundColor: color()}} className='rounded-full mr-2 w-[15px] h-[15px] bg-blue-100 '></span>
+        {c}</Link>
+    
+      ))}
 
-        <li className='flex items-center mb-3'>
-        <span className='rounded-full mr-2 w-[15px] h-[15px] bg-red-200 '></span>
-        Cat 2</li>
-
-        <li className='flex items-center mb-3'>
-        <span className='rounded-full mr-2 w-[15px] h-[15px] bg-green-100 '></span>
-        Cat 3</li>
-        
-      </ul>
+      </div>
     </nav>
   )
 }
